3dfbf069dcc4e701e1f86ba18891b16a6c580c89
Marco Scholten <ms@optimist-software.nl>
2023-06-22 15:07:53 +0200

    https

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
 	urls: [   
-        { url: "http://petstore.swagger.io/v2/swagger.json", name: "Pets" },  
+        { url: "https://petstore.swagger.io/v2/swagger.json", name: "Pets" },  
         { url: "openapi.json", name: "KLIC BMKL API" }  
     ],  
     "urls.primaryName": "KLIC BMKL API",     
